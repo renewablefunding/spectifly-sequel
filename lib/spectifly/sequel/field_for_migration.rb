c091eb149d74fcb9f28b7636cd47eecce4a4922b
Laurie <laurie@renewfund.com>
2013-05-07 14:22:55 -0700

    Only create an association table if a field refers to a many-to-many association, not if the entity's field is one-to-many.

--- lib/spectifly/sequel/field_for_migration.rb
+++ lib/spectifly/sequel/field_for_migration.rb
@@ -8,8 +8,10 @@ module Spectifly
         @field_name = field.name
         @field_type = field.type
         @model = model
-        entity = entity_references.detect { |e| e.root == field.type }
-        @model_for_field =  Spectifly::Sequel::Model.new entity, [] unless entity.nil?
+        if entity = entity_references.detect { |e| e.root == field.type }
+          builder = Spectifly::Sequel::Builder.new(entity)
+          @model_for_field =  Spectifly::Sequel::Model.new entity, builder.fields
+        end
         @options = retrieve_options(field)
         @field_as_foreign_key = ActiveSupport::Inflector.singularize(field.name) + '_id'
       end
@@ -28,9 +30,10 @@ module Spectifly
                      'single_value_entity_field'
                    elsif multiple_value_simple_type?
                      'multiple_value_simple_type_field'
-                   elsif multiple_value_entity?
-                     'multiple_value_entity_field'
+                   elsif many_to_many_association?
+                     'many_to_many_association'
                    end
+        return if template.nil?
 
         path = File.expand_path(File.join(*File.dirname(__FILE__), 'erb', 'field', "#{template}.erb"))
         content = File.read(path)
@@ -51,8 +54,11 @@ module Spectifly
         @model_for_field.nil? && @field.multiple?
       end
 
-      def multiple_value_entity?
-        !@model_for_field.nil? && @field.multiple?
+      def many_to_many_association?
+        !@model_for_field.nil? && @field.multiple? &&
+          # and the model being referenced has an attribute for the current model 
+          # and that model is multi-value (aka many-to-many rather than one to many)
+          @model_for_field.multiple_value_fields.map { |f| f.type }.include?(@model.display_name)
       end
     end
   end
--- lib/tasks/spectifly-sequel.rake
+++ lib/tasks/spectifly-sequel.rake
@@ -5,6 +5,7 @@ namespace 'spectifly' do
   namespace 'sequel' do
     desc 'Generate migration from entity definition file'
     task :generate, [:entity_type] do |t, args|
+      Spectifly::Sequel.configure_with File.join(Dir.pwd, 'config', 'spectifly.yml')
       Spectifly::Sequel::MigrationGenerator.new(args[:entity_type]).run!
     end
   end
